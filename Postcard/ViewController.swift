052f3bcd6224ebf88b49df2463918b994e096ec5
Russell Warner <russell@troped.com>
2014-11-20 22:01:50 -0500

    First commit of Postcard app

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var messageSignature: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var sendMailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageSignature.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.greenColor()
+        messageSignature.text = enterNameTextField.text
+        
+        enterMessageTextField.text = ""
+        enterNameTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        sender.backgroundColor = UIColor.grayColor()
+        sendMailButton.setTitle("Postcard Sent", forState: UIControlState.Normal)
+    }
 
 }
 
